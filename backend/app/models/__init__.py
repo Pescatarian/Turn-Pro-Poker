254eac75927b0cfdc7ab212e238afb663cdd195b
Pescatarian <itstrictlybiz@gmail.com>
2026-02-05 16:41:26 +0700

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
-"""Database models package."""
+"""Import all models here for Alembic to detect them."""
 from app.models.user import User
 from app.models.session import Session
 from app.models.transaction import Transaction
